dbc16b0acdce3493f9e553dc256042a45ff1f661
Livia Freire <livia1111@gmail.com>
2025-11-06 08:56:28 -0300

    Add functionality to repeat user input text

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,8 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Solicita um texto e um número do usuário
+texto = input("Digite um texto: ")
+quantidade = int(input("Digite um número inteiro: "))
+
+# Imprime o texto repetido conforme o número informado
+for i in range(quantidade):
+    print(texto)
